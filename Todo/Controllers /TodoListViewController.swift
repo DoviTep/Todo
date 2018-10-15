ca73ccc64eb7a552412b3359b1045058534e77d7
Dovi Tep <dovit@me.com>
2018-10-15 17:05:12 +0200

    Updated plist and functions to save

--- Todo/Controllers /TodoListViewController.swift
+++ Todo/Controllers /TodoListViewController.swift
@@ -11,19 +11,34 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     
-    var itemArray = ["Find Mike", "buy Eggs", "no shampoo at Checkers"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
- 
-
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) .first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoList") as? [String] {
-            
-            itemArray = items
         
-        }
+        
+        print(dataFilePath)
+      
+        let newItem = Item()
+            newItem.title = "Find Mike"
+            itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggs"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "destroy eggs"
+        itemArray.append(newItem3)
+        
+        
+  //      if let items = defaults.array(forKey: "TodoList") as? [Item] {
+  //          itemArray = items
+  //
+  //      }
         
         
     }
@@ -37,9 +52,15 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        print("cellForRowAtIndexpathcalled")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray [indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
         
         return cell
  
@@ -49,13 +70,10 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            
-        }
-    
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
@@ -70,12 +88,14 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style:  .default) { (action) in
             
-        self.itemArray.append(textfield.text!)
-            
-        self.defaults.set(self.itemArray, forKey: "TodoList")
         
+        let newItem = Item ()
+        newItem.title = textfield.text!
             
-        self.tableView.reloadData()
+        self.itemArray.append(newItem)
+            
+         self.saveItems()
+        
             
         }
         
@@ -84,16 +104,29 @@ class TodoListViewController: UITableViewController {
             textfield = alertTextField
             
             print(alertTextField)
-        
-            
-            
+    
         }
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
-        
- 
     }
+    func saveItems () {
+        
+        let encoder = PropertyListEncoder ()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error )")
+            
+            
+        }
+
+        self.tableView.reloadData()
+        
+    }
+    
 }
---
+++ Todo/DataModule/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todo
+//
+//  Created by Admin on 2018/10/15.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import Foundation
+
+    class Item: Encodable {
+        var title : String = ""
+        var done : Bool = false
+}
