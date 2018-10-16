d67553edbd3571697027c4ea01fa1b2b4db45aa9
Dovi Tep <dovit@me.com>
2018-10-16 12:27:08 +0200

    redo

--- Todo/AppDelegate.swift
+++ Todo/AppDelegate.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-@UIApplicationMain
+
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
@@ -44,11 +45,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-         print("applicationWillterminate")
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  
     }
 
+   
 
-}
+
+
 
